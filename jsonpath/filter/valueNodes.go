10ff34c36a3667eb1b1963a39cd8da45df74842c
CuiChao <any613@qq.com>
2022-04-02 21:27:01 +0800

    valueNode valueNodes

--- jsonpath/filter/valueNodes.go
+++ jsonpath/filter/valueNodes.go
@@ -1,1 +1,59 @@
 package filter
+
+import "cuichao.com/go-jsonpath/jsonpath"
+
+// PatternNode -------patternNode------
+type PatternNode struct {
+	valueNodeDefault
+	pattern string
+	flags   string
+}
+
+func (pn *PatternNode) IsPatternNode() bool {
+	return true
+}
+
+func (pn *PatternNode) AsPatternNode() (*PatternNode, *jsonpath.InvalidPathError) {
+	return pn, nil
+}
+
+// PathNode ------PathNode-----
+type PathNode struct {
+	valueNodeDefault
+}
+
+type NumberNode struct {
+	valueNodeDefault
+}
+
+type StringNode struct {
+	valueNodeDefault
+}
+
+type BooleanNode struct {
+	valueNodeDefault
+}
+
+type PredicateNode struct {
+	valueNodeDefault
+}
+
+type ValueListNode struct {
+	valueNodeDefault
+}
+
+type NullNode struct {
+	valueNodeDefault
+}
+
+type UndefinedNode struct {
+	valueNodeDefault
+}
+
+type ClassNode struct {
+	valueNodeDefault
+}
+
+type OffsetDateTimeNode struct {
+	valueNodeDefault
+}
