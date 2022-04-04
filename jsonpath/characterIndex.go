44f4c98f7c76c918ffad664c7314f4de35613046
CuiChao <any613@qq.com>
2022-04-04 09:50:44 +0800

    character index

--- jsonpath/characterIndex.go
+++ jsonpath/characterIndex.go
@@ -1,5 +1,7 @@
 package jsonpath
 
+import "strings"
+
 const (
 	OPEN_PARENTHESIS     rune = '('
 	CLOSE_PARENTHESIS    rune = ')'
@@ -17,6 +19,52 @@ const (
 
 type CharacterIndex struct {
 	charSequence string
-	position     int
-	endPosition  int
+	position     int64
+	endPosition  int64
+}
+
+func (ci *CharacterIndex) Length() int64 {
+	return ci.endPosition + 1
+}
+func (ci *CharacterIndex) CharAt(idx int64) rune {
+	return strings.IndexRune(ci.charSequence).charAt(idx)
+}
+
+func (ci *CharacterIndex) CurrentChar() {
+	return charSequence.charAt(position)
+}
+
+func (ci *CharacterIndex) CurrentCharIs(c rune) bool {
+	return (charSequence.charAt(position) == c)
+}
+
+func (ci *CharacterIndex) LastCharIs(char c) bool {
+	return charSequence.charAt(endPosition) == c
+}
+
+func (ci *CharacterIndex) NextCharIs(char c) bool {
+	return inBounds(position+1) && (charSequence.charAt(position+1) == c)
+}
+
+func (ci *CharacterIndex) IncrementPosition(charCount int64) int64 {
+	return ci.SetPosition(ci.position + charCount)
+}
+
+func (ci *CharacterIndex) DecrementEndPosition(charCount int64) int64 {
+	return ci.SetEndPosition(ci.endPosition - charCount)
+}
+
+func (ci *CharacterIndex) SetPosition(newPosition int64) int64 {
+	//position = min(newPosition, charSequence.length() - 1);
+	ci.position = newPosition
+	return ci.position
+}
+
+func (ci *CharacterIndex) SetEndPosition(newPosition int64) int64 {
+	ci.endPosition = newPosition
+	return ci.endPosition
+}
+
+func (ci *CharacterIndex) Position() int64 {
+	return ci.position
 }
