a182d1893e55bb14b2a09754bb79d8c9a74f7681
CuiChao <any613@qq.com>
2022-04-09 21:30:17 +0800

    path compiler

--- jsonpath/path/compiler.go
+++ jsonpath/path/compiler.go
@@ -1,8 +1,52 @@
 package path
 
-import "cuichao.com/go-jsonpath/jsonpath"
+import (
+	"cuichao.com/go-jsonpath/jsonpath"
+	"cuichao.com/go-jsonpath/jsonpath/filter"
+)
+
+const (
+	DOC_CONTEXT  = '$'
+	EVAL_CONTEXT = '@'
+
+	OPEN_SQUARE_BRACKET  = '['
+	CLOSE_SQUARE_BRACKET = ']'
+	OPEN_PARENTHESIS     = '('
+	CLOSE_PARENTHESIS    = ')'
+	OPEN_BRACE           = '{'
+	CLOSE_BRACE          = '}'
+
+	WILDCARD     = '*'
+	PERIOD       = '.'
+	SPACE        = ' '
+	TAB          = '\t'
+	CR           = '\r'
+	LF           = '\n'
+	BEGIN_FILTER = '?'
+	COMMA        = ','
+	SPLIT        = ':'
+	MINUS        = '-'
+	SINGLE_QUOTE = '\''
+	DOUBLE_QUOTE = '"'
+)
 
 type CompiledPath struct {
+	filterStack []filter.PredicateNode
+	path        *jsonpath.CharacterIndex
+}
+
+func (cp *CompiledPath) readWhitespace() {
+	for cp.path.InBounds() {
+		c := cp.path.CurrentChar()
+		if cp.isWhitespace(c) {
+			break
+		}
+		cp.path.IncrementPosition(1)
+	}
+}
+
+func (*CompiledPath) isWhitespace(c rune) bool {
+	return c == SPACE || c == TAB || c == LF || c == CR
 }
 
 func (cp *CompiledPath) Evaluate(document interface{}, rootDocument interface{}, configuration *jsonpath.Configuration) (jsonpath.EvaluationContext, error) {
